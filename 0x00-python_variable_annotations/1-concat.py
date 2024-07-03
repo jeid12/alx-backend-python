db6992845746eeaff3a36c5c8617988444ceb05e
jeid12 <niyokwizerajd123@gmail.com>
2024-07-03 08:23:39 +0000

    comited

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Basic Annotations - concat
+Write type-annotated function concat that takes two strings as argument
+Returns a concatenated string
+"""
+def concat(str1:str,str2:str) ->str:
+    
+    return str1+str2
